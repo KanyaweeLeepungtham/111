c5d2007b319d04a1409e09a41be580cdad8e3363
Kanyawee Leepungtham <kanyawee6799@gmail.com>
2025-09-19 13:46:51 +0700

    Update lab4_1.py

--- lab4_1.py
+++ lab4_1.py
@@ -1,14 +1,20 @@
-weight, max_score, std_score = [], [], []
-
-nums = input("Enter weights: ")
-weight.extend(map(int, nums.split()))
+max_score, std_score = [], []
+# weight=[]
+# nums = input("Enter weights: ")
+nums = input("Enter weight: ")
+weight=list(map(int, nums.split()))
 
 max_nums = input("Enter max scores: ")
-max_score.extend(map(int, max_nums.split()))
+max_score=list(map(int, max_nums.split()))
 
 std_nums = input("Enter student scores: ")
-std_score.extend(map(int, std_nums.split()))
+std_score=list(map(int, std_nums.split()))
 
-weighted_score = sum((std_score[i] / max_score[i]) * weight[i] for i in range(len(weight)))
+def calc_weighted_score():
+    if(weight==0):
+        weight = (max_score/3)*len(max_score)
+    weighted_score = sum((std_score[i] / max_score[i]) * weight[i] for i in range(len(weight)))
+    return weighted_score
+# final_score= calc_weighted_score
+print("Final weighted score is:", calc_weighted_score())
 
-print("Final weighted score is:", weighted_score)
